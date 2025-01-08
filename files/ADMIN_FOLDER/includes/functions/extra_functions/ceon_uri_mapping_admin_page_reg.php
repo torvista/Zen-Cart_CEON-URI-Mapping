f439f0eda6fd01c77030172da12c84ac9a729bed
torvista <4391026+torvista@users.noreply.github.com>
2025-01-08 13:45:38 +0100

    ceon_uri_mapping_admin_page_reg.php: prevent auto-deletion of admin reg. file

--- files/ADMIN_FOLDER/includes/functions/extra_functions/ceon_uri_mapping_admin_page_reg.php
+++ files/ADMIN_FOLDER/includes/functions/extra_functions/ceon_uri_mapping_admin_page_reg.php
@@ -13,23 +13,24 @@
  * @copyright   Portions Copyright 2003 osCommerce
  * @link        http://ceon.net/software/business/zen-cart/uri-mapping
  * @license     http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version     $Id: ceon_uri_mapping_admin_page_reg.php 1027 2012-07-17 20:31:10Z conor $
+ * @version     $Id: ceon_uri_mapping_admin_page_reg.php 2025-01-08 torvista
  */
 
 if (!defined('IS_ADMIN_FLAG')) {
 	die('Illegal Access');
 }
 
-// This file should normally only need to be run once, but if the user hasn't installed the software properly it
+// This file should normally only need to be run once, but if the user hasn't installed the software properly, it
 // may need to be run again. Flag tracks the situation
-$can_autodelete = true;
+// set autodelete to false: confusing when testing locally before copying files to server and this file is missing
+$can_autodelete = false;
 
 if (function_exists('zen_register_admin_page')) {
 	if (!zen_page_key_exists('ceon_uri_mapping_config')) {
 		// Register the Ceon URI Mapping Config Utility with the Zen Cart admin
 		
-		// Quick sanity check in case user hasn't uploaded a necessary file on which this depends
-		$error_messages = array();
+		// Quick sanity check in case the user hasn't uploaded a necessary file on which this depends
+		$error_messages = [];
 		
 		if (!defined('FILENAME_CEON_URI_MAPPING_CONFIG')) {
 			$error_messages[] = 'The Ceon URI Mapping filename define is missing. Please check that the file ' .
@@ -46,7 +47,7 @@ if (function_exists('zen_register_admin_page')) {
 					'Error: ' . $error_message . "</p>\n";
 			}
 		} else {
-			// Necessary file is in place so can register the admin page and have the menu item created
+			// The necessary file is in place so can register the admin page and have the menu item created
 			zen_register_admin_page('ceon_uri_mapping_config', 'BOX_CEON_URI_MAPPING',
 				'FILENAME_CEON_URI_MAPPING_CONFIG', '', 'modules', 'Y', 40);
 		}
@@ -56,5 +57,5 @@ if (function_exists('zen_register_admin_page')) {
 if ($can_autodelete) {
 	// Either the config utility file has been registered, or it doesn't need to be. Can stop the wasteful process
 	// of having this script run again by having it delete itself
-	@unlink(DIR_WS_INCLUDES . 'functions/extra_functions/ceon_uri_mapping_admin_page_reg.php');
+	unlink(DIR_WS_INCLUDES . 'functions/extra_functions/ceon_uri_mapping_admin_page_reg.php');
 }
